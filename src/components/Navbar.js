c5552f0315578027f734a4b6c6249f8d1ba8e2a9
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-24 18:00:34 +0600

    Navbar Created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import logo from '../image/Logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-        <ul>
+    <div className='navbar'>
+      <div className='logo'>
+        <img src={logo} alt="logo" width={130} height={26.33} />
+      </div>
+        <ul className='menu'>
             <li>Home</li>
             <li>About us</li>
             <li>What We Do</li>
-            <li>Home</li>
-            <li>Home</li>
+            <li>Media</li>
+            <li>Contact</li>
+            <li>
+            <button className='button'>Donate</button>
+            </li>
         </ul>
     </div>
   )
