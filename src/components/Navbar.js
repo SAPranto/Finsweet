a578da40856435236f2ebad9f09b0fca8deba42e
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-24 16:52:18 +0600

    Initial Commit With Navbar Setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>What We Do</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
