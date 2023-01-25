6b6a75ee55177f8b425fe21a719e30c37070fa7d
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-25 15:40:30 +0600

    Banner Created

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './Banner.css'
+import image from '../image/Image1.png'
 
 const Banner = () => {
   return (
+    <>
+    <div className='bnr-container'>
+      <span className='border-left'></span>
     <div className='banner'>
-      <h3>DONATE</h3>
+        <h3>Donate</h3>
       <h1>Making a donation for nature</h1>
       <p>When you donate, you’re supporting effective solutions to big environmental challenges—an investment in the future of our planet.</p>
       <button className='grn-btn'>Donate now</button>
     </div>
+      <img className='image' src={image} alt="Donatation" width={480} height={384} />
+    </div>
+    </>
   )
 }
 
