abebda647271b32173c1cbb9439634aa800f4749
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-26 05:08:05 +0600

    Made The Banner Component Reusable Using Props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar.js'
 import Banner from './components/Banner.js'
 import Footer from './components/Footer.js'
 import Body from './components/Body.js'
+import image from './image/Image1.png'
 
 const navItems = [
   { title: "Home", link: "/" },
@@ -17,7 +18,7 @@ function App() {
   return (
     <BrowserRouter>
     <Navbar navItems={navItems} />
-    <Banner/>
+    <Banner title="Donate" subtitle="Making a donation for nature" text="When you donate, you’re supporting effective solutions to big environmental challenges—an investment in the future of our planet." image={image}/>
     <Body/>
     <Footer/>
     </BrowserRouter>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import './Banner.css'
-import image from '../image/Image1.png'
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <>
     <div className='bnr-container'>
       <span className='border-left'></span>
     <div className='banner'>
-        <h3>Donate</h3>
-      <h1>Making a donation for nature</h1>
-      <p>When you donate, you’re supporting effective solutions to big environmental challenges—an investment in the future of our planet.</p>
+        <h3>{props.title}</h3>
+      <h1>{props.subtitle}</h1>
+      <p>{props.text}</p>
       <button className='grn-btn'>Donate now</button>
     </div>
-      <img className='image' src={image} alt="Donatation" width={480} height={384} />
+      <img className='image' src={props.image} alt={props.title} width={480} height={384} />
     </div>
     </>
   )
